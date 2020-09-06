69d8013919a2b88208e522fb7c3a75d63c0aefce
ogulyukin <o.gulyukin@yandex.ru>
2020-09-06 09:30:39 +0200

    Домашнее задание от 23.08.2020

--- ClassStudent.h
+++ ClassStudent.h
@@ -20,10 +20,14 @@ public:
     string group; //Группа
     int stage; //Курс
     bool study; //Учится?
+    int grades[5]; //Оценки
     void SetAge(int age); //Установка занчения возраста
     int GetAge(); //Получение значения возраста
+    double GetAverageGrade(); //Получить среднюю оценку
 private:
+    double averageGrade; //Средеяя оценка
     int age; //Возраст
+    void SetAverageGrade(); //расчет средней оценки
 };
 
 void Student::SetAge(int age)
@@ -38,4 +42,14 @@ void Student::SetAge(int age)
 int Student::GetAge()
 {
     return age;
+}
+double Student::GetAverageGrade() {
+    return averageGrade;
+}
+void Student::SetAverageGrade() {
+    int sum = 0;
+    for (int x = 0; x < 5; x++){
+        sum += averageGrade[x];
+    }
+    averageGrade = sum / 5;
 }
